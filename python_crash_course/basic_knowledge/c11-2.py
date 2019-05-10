0bc62111b635f3f6ac7918572ce470501e9a1857
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-05-10 16:18:05 +0800

    编辑

---
+++ python_crash_course/basic_knowledge/c11-2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=UTF-8
+'''
+@Description: S
+@Author: StaURL
+@WebSite: https://www.staurl.com
+@LastEditors: lzp
+@Date: 2019/5/10 15:04
+@LastEditTime: 15:04
+'''
+
+print("hello word")
+print("hello word")
